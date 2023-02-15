aa0067dd5fa2041740ab560e3aea383c1d5958bc
mmascaro864 <marc.j.mascaro@gmail.com>
2023-02-15 16:02:44 -0500

    more updates

--- albumapp/routes.py
+++ albumapp/routes.py
@@ -1,4 +1,4 @@
-from . import app
+from albumapp import app
 import json, plotly
 from flask import render_template
 from wrangling_scripts.wrangle_data import return_figures
--- myapp.py
+++ myapp.py
@@ -1,2 +1,1 @@
 from albumapp import app
-#app.run(host='0.0.0.0', port=3000, debug=True)
